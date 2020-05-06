9c107443286aa134ad2615a48abb40a559ee9ca4
Jackson <jacksonhopknins92@gmail.com>
2020-05-06 14:20:27 -0400

    typescript for router files

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { shelteredAnimal } from './routers/shelteredAnimal';
+import { species } from './routers/species';
+import { employeeKeeper } from './routers/employeeKeeper';
+
+console.log("hello test");

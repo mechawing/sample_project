4849e885f0c981edc56a6dd62c79f7dfccb7f3d1
Jackson <jacksonhopknins92@gmail.com>
2020-05-06 23:24:44 -0400

    Added 3 routers and wrote some code for shelteredAnimal.ts

--- src/routers/shelteredAnimal.ts
+++ src/routers/shelteredAnimal.ts
@@ -1,3 +1,36 @@
 import express from 'express';
 
 export const shelteredAnimal = express.Router();
+
+interface Animal { // Interface for an animal
+    shelterIDNumber: number;
+    name: string;
+    sex: string;
+};
+
+const animalResidents: Animal[] = [ // The current animals in the shelter
+    {
+        shelterIDNumber: 1,
+        name: 'Comet',
+        sex: 'Male',
+    },
+
+    {
+        shelterIDNumber: 2,
+        name: 'Lee',
+        sex: 'Male',
+    },
+
+    {
+        shelterIDNumber: 3,
+        name: 'Nova',
+        sex: 'Female',
+    }
+
+];
+
+shelteredAnimal.get('', (request, response, next) => { // Get function
+    response.json(animalResidents);
+    next();
+});
+

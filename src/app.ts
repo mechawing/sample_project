48aba685e68a48fbbd6e1962ea1703c937e93ca2
Jackson <jacksonhopknins92@gmail.com>
2020-05-06 23:20:27 -0400

    Added shetleredAnimal

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,19 @@ import { shelteredAnimal } from './routers/shelteredAnimal';
 import { species } from './routers/species';
 import { employeeKeeper } from './routers/employeeKeeper';
 
-console.log("hello test");
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+    next();
+});
+
+app.use('/animals', shelteredAnimal);
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`)
+});

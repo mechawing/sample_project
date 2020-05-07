7106f2ec1f23508ac120e7054fac89aa1bbb09b6
Jackson <jacksonhopknins92@gmail.com>
2020-05-07 13:12:29 -0400

    Added boolean value for declaw status to shelteredAnimal.ts

--- src/routers/shelteredAnimal.ts
+++ src/routers/shelteredAnimal.ts
@@ -6,6 +6,7 @@ interface Animal { // Interface for an animal
     shelterIDNumber: number;
     name: string;
     sex: string;
+    declawed: boolean; // While this procedure is done on cats most often, technically it can also be peformed on other animals as well.
 };
 
 const animalResidents: Animal[] = [ // The current animals in the shelter
@@ -13,18 +14,21 @@ const animalResidents: Animal[] = [ // The current animals in the shelter
         shelterIDNumber: 1,
         name: 'Comet',
         sex: 'Male',
+        declawed: false,
     },
 
     {
         shelterIDNumber: 2,
         name: 'Lee',
         sex: 'Male',
+        declawed: false,
     },
 
     {
         shelterIDNumber: 3,
         name: 'Nova',
         sex: 'Female',
+        declawed: false,
     }
 
 ];
